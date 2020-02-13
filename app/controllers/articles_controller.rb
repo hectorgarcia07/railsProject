dee0634bc087fbe6bf9ca112705e7afc9b264a0c
Hector Garcia <hector.garcia07@utrgv.edu>
2020-02-13 13:22:18 -0600

    finished blog commets feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,7 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article.id
     end
 end
